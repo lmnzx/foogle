0369566724b826382d9d1cb6bd82db23f2301280
Sayan Mallick <snyxmk@gmail.com>
2023-03-06 11:55:05 +0530

    basic lexer impelemtation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,111 @@
-use std::fs::File;
-use std::process::exit;
+use std::collections::HashMap;
+use std::fs::{self, File};
+use std::io;
+use std::path::Path;
 use xml::reader::{EventReader, XmlEvent};
 
-fn main() {
-    let file_path = "docs.gl/gl4/glClear.xhtml";
-    let file = File::open(file_path).unwrap_or_else(|err| {
-        eprintln!("ERROR: could not read file {file_path}::{err}");
-        exit(1)
-    });
-    let er = EventReader::new(file);
+#[derive(Debug)]
+struct Lexer<'a> {
+    //* lexer is just a reslicer
+    content: &'a [char],
+}
 
+impl<'a> Lexer<'a> {
+    fn new(content: &'a [char]) -> Self {
+        Self { content }
+    }
+
+    fn trim_left(&mut self) {
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            self.content = &self.content[1..]
+        }
+    }
+
+    fn chop(&mut self, n: usize) -> &'a [char] {
+        let token = &self.content[0..n];
+        self.content = &self.content[n..];
+        token
+    }
+
+    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char]
+    where
+        P: FnMut(&char) -> bool,
+    {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+        self.chop(n)
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        self.trim_left();
+        if self.content.len() == 0 {
+            return None;
+        }
+
+        if self.content[0].is_numeric() {
+            return Some(self.chop_while(|x| x.is_numeric()));
+        }
+
+        if self.content[0].is_alphabetic() {
+            return Some(self.chop_while(|x| x.is_alphanumeric()));
+        }
+
+        return Some(self.chop(1));
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = &'a [char];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
+    }
+}
+
+fn index_document(content: &str) -> HashMap<String, usize> {
+    todo!();
+}
+
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
+    let file = File::open(file_path)?;
+    let er = EventReader::new(file);
     let mut content = String::new();
 
     for event in er.into_iter() {
-        let event = event.unwrap_or_else(|err| {
-            eprintln!("ERROR: could not read next XML event: {err}");
-            exit(1)
-        });
-        if let XmlEvent::Characters(text) = event {
-            content.push_str(&text)
+        if let XmlEvent::Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+            content.push_str(" ");
         }
-        println!("{content}")
     }
+
+    Ok(content)
+}
+
+fn main() -> io::Result<()> {
+    let content = read_entire_xml_file("docs.gl/gl4/glTransformFeedbackVaryings.xhtml")?
+        .chars()
+        .collect::<Vec<_>>();
+
+    for token in Lexer::new(&content) {
+        println!(
+            "{token}",
+            token = token
+                .into_iter()
+                .map(|x| x.to_ascii_uppercase())
+                .collect::<String>()
+        );
+    }
+
+    // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
+
+    // let dir_path = "docs.gl/gl4/";
+    // let dir = fs::read_dir(dir_path)?;
+    // for file in dir {
+    //     let file_path = file?.path();
+    //     let content = read_entire_xml_file(&file_path)?;
+    //     println!("{file_path:?} => {size}", size = content.len())
+    // }
+    Ok(())
 }
